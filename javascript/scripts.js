3f71f72d50b641aa151f03846c8cafb0940cb255
Elisabetta1 <elisabetta.verginelli@gmail.conm>
2019-11-21 15:17:45 +0000

    home page and navbar styling + JS + renamed images
    Split homepage in 3 sections, added backgrounds and info, scroll to top button. Added logo on navbar. Renamed images with easy-to understand names instead of numbers.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,13 @@
+// FadeIn effect on home page load
 $(document).ready(function(){
   console.log('log');
-  /*! Fades in h1 on load */
-  // $('#welcome').fadeOut('slow');
-  $('#cover').fadeOut(3000);
+  $('#cover').fadeOut(2000);
+});
+
+// Scroll to top function
+jQuery('.scroll_to').click(function(e){
+    var jump = $(this).attr('href');
+    var new_position = $(jump).offset();
+    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
+    e.preventDefault();
 });
