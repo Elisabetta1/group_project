ef5e41cad0215b183b8ae3f0cfdda15f4784e99e
Elisabetta1 <elisabetta.verginelli@gmail.conm>
2019-11-25 16:59:32 +0000

    Added link to index.html in navbar logo, styled scroll-to-top button and slowed speed

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
 });
 
 // Scroll to top function
-jQuery('.scroll_to').click(function(e){
-    var jump = $(this).attr('href');
-    var new_position = $(jump).offset();
-    $('html, body').stop().animate({ scrollTop: new_position.top }, 500);
-    e.preventDefault();
+$('.scroll_to').click(function () {
+    $('body').animate(
+      {
+        scrollTop: 0
+      },
+      "smooth"
+    );
 });
